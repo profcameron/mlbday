f939b9a9fc71b92bf54c338d37e3d065741cee05
Eric Cameron <ericcameronnj@gmail.com>
2024-08-05 00:22:26 -0400

    Adding dependencies for ical4j to generate .ics file; add sample ICS files to main>java>resources folder

---
+++ src/main/java/dev/ericcameron/PlayerDateExplorer.java
@@ -0,0 +1,22 @@
+package dev.ericcameron;
+
+import java.util.List;
+
+public class PlayerDateExplorer {
+
+    /**
+     * Generates CSV content from a list of PlayerModel objects.
+     *
+     * @param players A list of PlayerModel objects containing player data
+     * @return A string representing the CSV content of the player data
+     */
+    public String generatePlayerCsv(List<PlayerModel> players) {
+        // Method implementation will go here
+        return null;
+    }
+
+    public void createICSFile(List<PlayerModel> players) {
+        // Method implementation will go here
+        // Can be validated using https://icalendar.org/validator.html
+    }
+}
